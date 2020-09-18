f191829c1bd1c1e9b569817a47f898e418735d42
Micah Ellis <micahsellis@gmail.com>
2020-09-17 20:29:10 -0600

    first commit

---
+++ main_app/urls.py
@@ -0,0 +1,35 @@
+"""dogcollector URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name = 'home'),
+    path('about/', views.about, name ='about'),
+    path('dogs/<int:dog_id>/', views.dogs_detail, name='detail'),
+    path('dogs/create/', views.DogCreate.as_view(), name = 'dogs_create'),
+    path('dogs/<int:pk>/update/', views.DogUpdate.as_view(), name='dogs_update'),
+    path('dogs/<int:pk>/delete/', views.DogDelete.as_view(), name = 'dogs_delete'),
+    path('dogs/<int:dog_id>/add_feeding/',
+         views.add_feeding, name = 'add_feeding'),
+    path('dogs/<int:dog_id>/assoc_toy/<int:toy_id>/',
+         views.assoc_toy, name='assoc_toy'),
+    path('toys/', views.ToyList.as_view(), name='toys_index'),
+    path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
+    path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
+    path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
+    path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
+]
