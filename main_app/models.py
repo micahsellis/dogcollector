8107ac05905b079dcc2e656c3172ae469e7dada7
Micah Ellis <micahsellis@gmail.com>
2020-09-21 10:34:26 -0600

    remove User

--- main_app/models.py
+++ main_app/models.py
@@ -26,7 +26,6 @@ class Dog(models.Model):
     description = models.TextField(max_length=250)
     age = models.IntegerField()
     toys = models.ManyToManyField(Toy)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
     def __str__(self):
